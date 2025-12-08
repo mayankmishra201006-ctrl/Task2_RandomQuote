f251b4cae985364ea9ca4057a551fb79815d650a
mayankmishra201006-ctrl <mayankmishra201006@gmail.com>
2025-12-08 08:25:35 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let quotes = [
+    {
+        text: "The best way to get started is to quit talking and begin doing.",
+        author: "Walt Disney"
+    },
+    {
+        text: "It's not whether you get knocked down, it's whether you get up.",
+        author: "Vince Lombardi"
+    },
+    {
+        text: "The future belongs to those who believe in the beauty of their dreams.",
+        author: "Eleanor Roosevelt"
+    }
+];
+
+function generateQuote() {
+    let randomNumber = Math.floor(Math.random() * quotes.length);
+    document.getElementById("quote").innerText = quotes[randomNumber].text;
+    document.getElementById("author").innerText = "- " + quotes[randomNumber].author;
+}
